8e9c6cae2996ae20236e343e1916d2c36f6e3db4
Daniel Benamy <daniel@benamy.info>
2013-04-01 23:33:12 -0400

    Added basic server storage with google app engine.

---
+++ app.py
@@ -0,0 +1,90 @@
+﻿import logging
+
+from google.appengine.api import users
+from google.appengine.ext import db
+import webapp2
+from webapp2_extras import json
+
+from models import Value
+import settings
+
+
+def require_auth(func):
+    """A decorator."""
+    def new_func(self):
+        user = users.get_current_user()
+        if not user:
+            self.response.headers.add_header('content-type', 'application/json', charset='utf-8')
+            self.response.out.write(json.encode({
+                'status': 'NEED_LOGIN',
+                'url': users.create_login_url("/"),
+            }))
+        elif user.user_id() not in settings.AUTHORIZED_USER_IDS:
+            logging.warn("User %s tried to access the server and isn't authorized." % user.user_id())
+            self.response.headers.add_header('content-type', 'application/json', charset='utf-8')
+            self.response.out.write(json.encode({
+                'status': 'NOT_AUTHORIZED',
+                'userId': user.user_id(),
+            }))
+        else:
+            func(self)
+    return new_func
+
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.redirect('client/index.html')
+
+
+class StorageApi(webapp2.RequestHandler):
+    @require_auth
+    def get(self):
+        task_storage = self.get_storage()
+        if self.request.get('forceDownload') == 'true':
+            content_type = 'application/octet-stream'
+        else:
+            content_type = 'application/json'
+        self.response.headers.add_header('content-type', content_type, charset='utf-8')
+        self.response.out.write(json.encode({
+            'status': 'OK',
+            'value': task_storage.value,
+            'lastSavedVersion': task_storage.last_saved_version,
+        }))
+
+    @require_auth
+    def post(self):
+        # TODO prevent CSRF by requiring a token provided by get()
+        resp = self.save_value()
+        self.response.headers.add_header('content-type', 'application/json', charset='utf-8')
+        self.response.out.write(json.encode(resp))
+
+    def get_storage(self):
+        user_id = users.get_current_user().user_id()
+        task_storage = db.get(db.Key.from_path('Value', str(user_id)))
+        if task_storage is None:
+            task_storage = Value(key_name=str(user_id), value='', last_saved_version=0)
+        return task_storage
+
+    @db.transactional
+    def save_value(self):
+        value = self.request.get('value')
+        last_saved_version = self.request.get('lastSavedVersion')
+        if value == '' or last_saved_version == '':
+            return {'status': 'MISSING_ARG'}
+        task_storage = self.get_storage()
+        if str(task_storage.last_saved_version) != last_saved_version:
+            return {'status': 'DATA_CHANGED'}
+        if task_storage.value != value:
+            task_storage.value = value
+            task_storage.last_saved_version += 1
+            task_storage.put()
+        return {
+            'status': 'OK',
+            'lastSavedVersion': task_storage.last_saved_version,
+        }
+
+
+app = webapp2.WSGIApplication([
+    ('/api/storage', StorageApi),
+    ('/', MainPage),
+], debug=settings.DEBUG)
---
+++ client/sync.js
@@ -0,0 +1,142 @@
+angular.module('sync', ['storage', 'net']).service('sync', function ($rootScope, $timeout, storage, net) {
+
+	var status = {
+		syncing: false,
+		text: ''
+	};
+
+	var syncTimer = null;
+
+	// TODO handle errors uploading and downloading
+	var sync = function() {
+		if (status.syncing) {
+			return;
+		}
+		$timeout.cancel(syncTimer);
+		status.syncing = true;
+		status.text = "Syncing...";
+		net.downloadData(function(str) {
+			_mergeServerData(str);
+			var upload = {
+				tasks: storage.tasks
+				// tags: data.tags
+			};
+			net.uploadData(JSON.stringify(upload), syncDone);
+		});
+	};
+
+	$rootScope.$watch(storage.tasks, sync, true);
+
+	var _mergeServerData = function(str) {
+		var json = {tasks: []}; //, tags: []};
+		if (str !== '') {
+			json = JSON.parse(str);
+		}
+		_mergeTasks(storage.tasks, json.tasks);
+		// _mergeTags(data.tags, json.tags);
+	};
+
+	/**
+	 * Merges serverTasks into clientTasks, modifying clientTasks
+	 * clientTasks should be the array of tasks in the app.
+	 * serverTasks should be the array of tasks downloaded from the server.
+	 */
+	var _mergeTasks = function(clientTasks, serverTasks) {
+		console.log('Merging tasks:');
+		console.log(clientTasks);
+		console.log(serverTasks);
+		var serverTasksById = _tasksById(serverTasks);
+		$.each(clientTasks, function(i, task) {
+			console.log('Looking at client task ' + i);
+			console.log(task);
+			if (task.id in serverTasksById) {
+				var serverTask = serverTasksById[task.id];
+				delete serverTasksById[task.id];
+				// Skip fancy merging for now and just use all data from most recently updated.
+				// $.each(['text', 'done'], function(id, field) {
+				// 	if (serverTask[field].lastUpdated > task[field].lastUpdated) {
+				// 		task[field] = serverTask[field];
+				// 	}
+				// });
+				// var tags = task.get('tags');
+				// $.each(tags, function(tag, info) {
+				// 	if (tag in serverTask.tags) {
+				// 		if (serverTask.tags[tag].lastUpdated > info.lastUpdated) {
+				// 			tags[tag] = serverTask.tags[tag];
+				// 		}
+				// 		delete serverTask.tags[tag];
+				// 	}
+				// });
+				// // Any tags left in serverTask.tags don't exist in (client) task and
+				// // should be added.
+				// $.each(serverTask.tags, function(tag, info) {
+				// 	tags[tag] = info;
+				// });
+				// task.set({tags: tags});
+				if (serverTask.lastUpdated > task.lastUpdated) {
+					task.text = serverTask.text;
+					task.tags = serverTask.tags;
+					task.done = serverTask.done;
+					task.lastUpdated = serverTask.lastUpdated;
+				}
+			}
+		});
+		// Anything left in serverTasksById doesn't exist in clientTasks and
+		// should be added to the result.
+		$.each(serverTasksById, function(id, serverTask) {
+			clientTasks.push(serverTask);
+		});
+		console.log('Merged. New tasks:');
+		console.log(clientTasks);
+	};
+
+	var _tasksById = function(tasks) {
+		var res = {};
+		$.each(tasks, function(i, task) {
+			res[task.id] = task;
+		});
+		return res;
+	};
+
+	// /**
+	//  * Merges serverTags into clientTags, modifying clientTags.
+	//  */
+	// _mergeTags = function(clientTags, serverTags) {
+	// 	console.log('Merging tags:');
+	// 	console.log(clientTags);
+	// 	console.log(serverTags);
+	// 	var serverTagsByName = {};
+	// 	$.each(serverTags, function(i, tag) {
+	// 		serverTagsByName[tag.name] = tag;
+	// 	});
+	// 	clientTags.each(function(tag) {
+	// 		if (tag.get('name') in serverTagsByName) {
+	// 			var serverTag = serverTagsByName[tag.get('name')];
+	// 			delete serverTagsByName[tag.get('name')];
+	// 			if (serverTag.lastUpdated > tag.get('lastUpdated')) {
+	// 				tag.set(serverTag);
+	// 			}
+	// 		}
+	// 	});
+	// 	$.each(serverTagsByName, function(name, tag) {
+	// 		clientTags.add(tag, {silent: true});
+	// 	});
+	// 	clientTags.trigger('add');
+	// 	console.log('Merged. New tags:');
+	// 	console.log(clientTags);
+	// };
+
+	var syncDone = function() {
+		$rootScope.$apply(function() {
+			status.syncing = false;
+			status.text = "Last synced at " + (new Date()); // TODO make this prettier
+			syncTimer = $timeout(sync, 30000);
+		});
+	};
+
+	return {
+		sync: sync,
+		status: status,
+		downloadAsFile: net.downloadAsFile
+	};
+});

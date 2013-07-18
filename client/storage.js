bdba2707dc688da805c1c1749de879a5b607442e
Daniel Benamy <daniel@benamy.info>
2013-07-18 10:40:59 -0400

    Moved generateId to storage & made it normalize tags on load.

--- client/storage.js
+++ client/storage.js
@@ -25,14 +25,22 @@ angular.module('goodoo').service('storage', function($rootScope, $timeout) {
 		) / 1000;
 	};
 
+	var nextId = 0;
+	var generateId = function() {
+		// The timestamp is the basis of the unique id. The nextId counter is in case the clock shifts for daylight
+		// savings, a time correction, etc. The random number is added just in case somehow tasks are created at the
+		// same time, with the same nextId, on two different clients.
+		return sprintf('%s-%s-%s', Date.now(), nextId++, Math.round(Math.random() * 1000));
+	};
+
 	var load = function() {
 		var gooDooData = JSON.parse(localStorage.goodoo || '{}');
 		if (!gooDooData.tasks) {
 			gooDooData.tasks = [
 				{
 					id: 'sample-1',
-					tags: ['#@Computer', '#GooDoo'],
-					text: "A task with 2 tags",
+					tags: ['#@Computer', '#BuyInsurance'],
+					text: "Get quotes on http://www.ehealthinsurance.com",
 					done: false,
 					updated_at: utcTs()
 				},
@@ -46,16 +54,32 @@ angular.module('goodoo').service('storage', function($rootScope, $timeout) {
 			];
 		}
 		if (!gooDooData.tags) {
-			gooDooData.tags = [
+			gooDooData.tags = [ // missing attributes will get added in normalizeTags
 				{text: '@Phone'},
-				{text: 'GooDoo'},
-				{text: '@Computer'}
+				{text: '@Computer'},
+				{text: 'UnpackBoxes'},
+				{text: 'BuyInsurance'}
 			];
 		}
+		normalizeTags(gooDooData.tags);
 		console.log("Loaded:");
 		console.log(gooDooData);
 		$rootScope.tasks = gooDooData.tasks; // all tasks, done and remaining
 		$rootScope.tags = gooDooData.tags;
+	};
+
+	var normalizeTags = function(tags) {
+		angular.forEach(tags, function(tag) {
+			if (!('id' in tag)) { // TODO i think there's something safer than not in
+				tag.id = generateId();
+			}
+			if (!('deleted' in tag)) {
+				tag.deleted = false;
+			}
+			if (!('lastUpdated' in tag)) {
+				tag.lastUpdated = utcTs();
+			}
+		});
 	};
 
 	load();
@@ -73,6 +97,7 @@ angular.module('goodoo').service('storage', function($rootScope, $timeout) {
 		tasks: $rootScope.tasks,
 		tags: $rootScope.tags,
 		searchStr: $rootScope.searchStr,
-		utcTs: utcTs
+		utcTs: utcTs,
+		generateId: generateId
 	};
 });

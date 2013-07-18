a588d6ecb98aca15d2adb034b91f07f0be41fa87
Daniel Benamy <daniel@benamy.info>
2013-07-18 11:01:24 -0400

    Renamed labels ctrl to tags ctrl for consistency.

--- client/tags_ctrl.js
+++ client/tags_ctrl.js
@@ -1,4 +1,4 @@
-angular.module('goodoo').controller('LabelsCtrl', function($scope, storage) {
+angular.module('goodoo').controller('TagsCtrl', function($scope, storage) {
 	$scope.tasks = storage.tasks; // watching storage.tasks directly doesn't work
 	$scope.tags = storage.tags;
 	$scope.searchStr = storage.searchStr;
